78b2fbf70b013c3b99a358004c1a7a85e36e6b56
MrTihon <tihonsomov@mail.ru>
2022-10-16 16:39:43 +0300

    add Task_0038

---
+++ Task_0038/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] CreateArrayRnddouble(int size, int min, int max)
+{
+    var array = new double[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double rndnum =rnd.NextDouble()*(max - min) + min;
+        array[i] = Math.Round(rndnum,1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}. ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < array.Length; z++)
+{
+    if (array[z] > max)
+        {
+            max = array[z];
+        }
+    if (array[z] < min)
+        {
+            min = array[z];
+        }
+}
+double resault = max - min;
+double res = Math.Round(resault,1);
+Console.WriteLine($" -> {res}");
+}
+
+
+double[] arr1 = CreateArrayRnddouble(5, 1, 100);
+PrintArray(arr1);
